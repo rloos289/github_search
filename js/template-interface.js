17aec57065730ddbd99395de9b0c0911878b9e8d
Ryan Loos <RLoos289@gmail.com>
2016-10-13 17:28:45 -0700

    set up basic file structure and README

---
+++ js/template-interface.js
@@ -0,0 +1,15 @@
+// var template = require('./../js/template.js').TemplateModule;
+//
+// exports.TemplateModule = Template;
+
+//<!-- Back End -->
+console.log('basic javascript is functioning')
+
+$(document).ready(function(){
+if (jQuery) {
+console.log('jQuery 3.1.0 is loaded');
+} else {
+console.log('jQuery is not loaded');
+    }
+});
+
